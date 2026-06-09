13643ceb404cba7e5f2a106729550fb6c93d0d46
ikramullah35 <ikramullah4225@gmail.com>
2026-06-09 13:40:26 -0700

    Refactor SocialLinks component: remove iconUrl property and update BrandIcon rendering logic for improved performance and maintainability

--- app/components/SocialLinks.tsx
+++ app/components/SocialLinks.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { useState } from "react";
 
 type Social = {
   name: string;
   icon: "whatsapp" | "youtube" | "tiktok" | "snapchat" | "facebook" | "telegram";
   color: string;
   href: string;
-  iconUrl: string;
 };
 
 export const socialLinks: Social[] = [
@@ -15,42 +13,36 @@ export const socialLinks: Social[] = [
     icon: "whatsapp",
     color: "#25d366",
     href: "https://whatsapp.com/channel/0029VbCjjwmCMY09BY3ABO1T",
-    iconUrl: "https://cdn.simpleicons.org/whatsapp/25D366",
   },
   {
     name: "YouTube",
     icon: "youtube",
     color: "#ff0000",
     href: "https://youtube.com/@universallinkonlineeducation",
-    iconUrl: "https://cdn.simpleicons.org/youtube/FF0000",
   },
   {
     name: "TikTok",
     icon: "tiktok",
     color: "#010101",
     href: "https://www.tiktok.com/@universal.link3",
-    iconUrl: "https://cdn.simpleicons.org/tiktok/010101",
   },
   {
     name: "Snapchat",
     icon: "snapchat",
     color: "#fffc00",
     href: "https://www.snapchat.com/add/universallink26",
-    iconUrl: "https://cdn.simpleicons.org/snapchat/111111",
   },
   {
     name: "Facebook",
     icon: "facebook",
     color: "#1877f2",
     href: "https://www.facebook.com/share/1LTRJtsD5C/",
-    iconUrl: "https://cdn.simpleicons.org/facebook/1877F2",
   },
   {
     name: "Telegram",
     icon: "telegram",
     color: "#0088cc",
     href: "https://t.me/universallink",
-    iconUrl: "https://cdn.simpleicons.org/telegram/0088CC",
   },
 ];
 
@@ -63,59 +55,33 @@ export function BrandIcon({
   color: string;
   size?: number;
 }) {
-  const [failed, setFailed] = useState(false);
-  const social = socialLinks.find((item) => item.icon === name);
-
-  if (!social) {
-    return null;
-  }
-
-  if (name === "telegram" || failed) {
-    return <FallbackBrandIcon name={name} color={color} size={size} />;
-  }
-
-  return (
-    <img
-      src={social.iconUrl}
-      alt=""
-      aria-hidden="true"
-      width={size}
-      height={size}
-      onError={() => setFailed(true)}
-      style={{
-        display: "block",
-        width: size,
-        height: size,
-        objectFit: "contain",
-      }}
-    />
-  );
-}
-
-function FallbackBrandIcon({
-  name,
-  color,
-  size,
-}: {
-  name: Social["icon"];
-  color: string;
-  size: number;
-}) {
   const common = {
     width: size,
     height: size,
     viewBox: "0 0 48 48",
     fill: "none",
     xmlns: "http://www.w3.org/2000/svg",
-    "aria-hidden": true,
+    "aria-hidden": true as const,
     style: { display: "block" },
   };
 
   switch (name) {
-    case "telegram":
+    case "whatsapp":
       return (
         <svg {...common}>
-          <path fill="#0088cc" d="M24 4C12.96 4 4 12.96 4 24s8.96 20 20 20 20-8.96 20-20S35.04 4 24 4Zm9.04 14.04-3.12 14.72c-.24 1.04-.84 1.28-1.68.8l-4.64-3.44-2.24 2.16c-.24.24-.44.44-.92.44l.32-4.68 8.52-7.68c.36-.32-.08-.52-.56-.2l-10.52 6.64-4.56-1.44c-1-.32-1.02-1 .2-1.48l17.8-6.88c.84-.28 1.56.2 1.28 1.28Z" />
+          <path fill="#25D366" d="M24 4C12.96 4 4 12.96 4 24c0 4.08 1.28 7.86 3.46 10.96L5.4 42.6l7.84-2.04A19.9 19.9 0 0 0 24 44c11.04 0 20-8.96 20-20S35.04 4 24 4Zm9.2 28.6c-.36 1.04-2.1 2-3.44 2.24-.88.16-2.04.24-3.32-.2-1.28-.44-7.04-3.24-9.92-8.72-2.88-5.48-1.68-9.12-.84-10.48.84-1.36 1.84-1.48 2.4-1.48.56 0 1.12.04 1.6.08.52.04 1.2-.2 1.88 1.44.68 1.64 2.32 5.64 2.52 6.04.2.4.32.88.08 1.4-.24.52-.36.76-.72 1.16-.36.4-.76.88-1.08 1.2-.36.32-.72.68-.32 1.32.4.64 1.8 3 3.88 4.88 2.68 2.4 4.6 2.96 5.36 3.2.76.24 1.2.2 1.64-.12.44-.32 2.04-2 2.6-2.68.56-.68 1.12-.56 1.88-.32.76.24 4.8 2.36 5.64 2.8.84.44 1.4.64 1.6 1 .2.36.2 2.08-.16 3.12Z" />
+        </svg>
+      );
+    case "youtube":
+      return (
+        <svg {...common}>
+          <path fill="#FF0000" d="M43.2 14.4c-.48-1.8-1.92-3.24-3.72-3.72C36.04 10 24 10 24 10s-12.04 0-15.48.68c-1.8.48-3.24 1.92-3.72 3.72C4 17.84 4 24 4 24s0 6.16.8 9.6c.48 1.8 1.92 3.24 3.72 3.72C11.96 38 24 38 24 38s12.04 0 15.48-.68c1.8-.48 3.24-1.92 3.72-3.72.8-3.44.8-9.6.8-9.6s0-6.16-.8-9.6ZM19.6 30.4V17.6l10.4 6.4-10.4 6.4Z" />
+        </svg>
+      );
+    case "tiktok":
+      return (
+        <svg {...common}>
+          <path fill="#010101" d="M34.2 12.8a8.86 8.86 0 0 1-2.8-6.8h-4.4v24.4c0 2.8-2.2 5-5 5s-5-2.2-5-5 2.2-5 5-5c.4 0 .8 0 1.2.2v-4.6c-.4-.04-.8-.08-1.2-.08-5.2 0-9.4 4.2-9.4 9.4s4.2 9.4 9.4 9.4 9.4-4.2 9.4-9.4V19.2c2 1.4 4.4 2.4 7.2 2.4v-4.4c-1.6 0-3.2-.6-4.4-1.6Z" />
         </svg>
       );
     case "snapchat":
@@ -124,10 +90,16 @@ function FallbackBrandIcon({
           <path fill="#FFFC00" stroke="#111" strokeWidth="1.8" d="M24 8.5c5.05 0 8.25 3.55 8.25 8.9 0 1.65-.2 3.1-.25 4.25.95.65 2.1.98 3.4 1.05-.95 1.65-2.3 2.65-4.05 2.95.9 2.8 3 5 6.35 6.65-2.75.35-4.65.98-5.7 1.95-1.25 1.15-2.75-.25-4.7.18-1.15.25-2 1-3.3 1s-2.15-.75-3.3-1c-1.95-.43-3.45.97-4.7-.18-1.05-.97-2.95-1.6-5.7-1.95 3.35-1.65 5.45-3.85 6.35-6.65-1.75-.3-3.1-1.3-4.05-2.95 1.3-.07 2.45-.4 3.4-1.05-.05-1.15-.25-2.6-.25-4.25 0-5.35 3.2-8.9 8.25-8.9Z" />
         </svg>
       );
-    case "whatsapp":
+    case "facebook":
       return (
-        <svg {...common} fill={color} viewBox="0 0 16 16">
-          <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93a7.9 7.9 0 0 0-2.327-5.607ZM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592Zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232Z" />
+        <svg {...common}>
+          <path fill="#1877F2" d="M24 4C12.96 4 4 12.96 4 24c0 9.92 7.28 18.16 16.8 19.64V29.8h-5.04V24h5.04v-4.36c0-5 2.96-7.72 7.48-7.72 2.16 0 4.44.4 4.44.4v4.88h-2.5c-2.48 0-3.24 1.52-3.24 3.08V24h5.52l-.88 5.8h-4.64v13.84C36.72 42.16 44 33.92 44 24c0-11.04-8.96-20-20-20Z" />
+        </svg>
+      );
+    case "telegram":
+      return (
+        <svg {...common}>
+          <path fill="#0088cc" d="M24 4C12.96 4 4 12.96 4 24s8.96 20 20 20 20-8.96 20-20S35.04 4 24 4Zm9.04 14.04-3.12 14.72c-.24 1.04-.84 1.28-1.68.8l-4.64-3.44-2.24 2.16c-.24.24-.44.44-.92.44l.32-4.68 8.52-7.68c.36-.32-.08-.52-.56-.2l-10.52 6.64-4.56-1.44c-1-.32-1.02-1 .2-1.48l17.8-6.88c.84-.28 1.56.2 1.28 1.28Z" />
         </svg>
       );
     default:
